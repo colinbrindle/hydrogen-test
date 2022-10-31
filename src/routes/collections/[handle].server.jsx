499035cfac61b26eba1e0eb7ee9fc5f792559c1a
colin brindle <hcolinbrindle@gmail.com>
2022-10-31 11:21:22 -0400

    Updated graphQL query and built collections page

--- src/routes/collections/[handle].server.jsx
+++ src/routes/collections/[handle].server.jsx
@@ -11,6 +11,8 @@ import { Suspense } from "react";
 
 import { Layout } from "../../components/Layout.server";
 
+import ProductCard from "../../components/ProductCard.server";
+
 export default function Collection() {
     const { handle } = useRouteParams();
 
@@ -32,11 +34,6 @@ export default function Collection() {
 
     return (
         <Layout>
-
-            {/* <section className="p-6 md:p-8 lg:p-12">
-                This will be the collection page for <strong>{collection.title}</strong>
-            </section> */}
-            
             <Suspense>
                 <Seo type="collection" data={collection} />
             </Suspense>
@@ -56,6 +53,14 @@ export default function Collection() {
                 </div>
                 )}
             </header>
+
+            <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+                <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    {collection.products.nodes.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+                </div>
+            </section>
             
         </Layout>
     );
@@ -72,15 +77,50 @@ export default function Collection() {
 // `;
 
 // Seo component will use collections' seo values when specified. Seo component must be wrapped within collection query
+
 const QUERY = gql`
     query CollectionDetails($handle: String!) {
         collection(handle: $handle) {
             id
             title
             description
-        seo {
-            description
-            title
-        }
+            seo {
+                description
+                title
+            }
+            image {
+                id
+                url
+                width
+                height
+                altText
+            }
+            products(first: 8) {
+                nodes {
+                    id
+                    title
+                    publishedAt
+                    handle
+                    variants(first: 1) {
+                        nodes {
+                            id
+                            image {
+                                url
+                                altText
+                                width
+                                height
+                            }
+                            priceV2 {
+                                amount
+                                currencyCode
+                            }
+                            compareAtPriceV2 {
+                                amount
+                                currencyCode
+                            }
+                        }
+                    }
+                }
+            }
     }
 }`;
